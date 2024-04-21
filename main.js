b7b34754ccdc595e1c1a612cea19cb6c883f15ed
Musawer Khan <musawerbangash109@gmail.com>
2024-04-22 04:52:07 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+//con-catenation method
+let string1 = "My name is Musawer Bangash ";
+let string2 = "But my name in cnic is Musawer Khan";
+//Combines the text of two strings and  
+// returns a new string. 
+console.log("Con Catenated String:" + string1.concat(string2.toString()));
+//Another Example
+let firstname = "Musawer";
+let lastname = "Bangash";
+let FullName = firstname + ' ' + lastname;
+console.log(FullName);
---
+++ main.ts
@@ -0,0 +1,16 @@
+        //con-catenation method
+
+        let string1:string = "My name is Musawer Bangash ";
+        let string2:string = "But my name in cnic is Musawer Khan";
+           
+          //Combines the text of two strings and  
+          // returns a new string. 
+        console.log("Con Catenated String:" +string1.concat(string2.toString()));
+      
+
+        //Another Example
+
+        let firstname:string = "Musawer";
+         let lastname:string = "Bangash";
+         let fullName:string = firstname + ' ' + lastname;
+         console.log(fullName);
